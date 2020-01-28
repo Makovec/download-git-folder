cfe2643b4b86da6b0fc0b1959e8da063f522ae50
David Moravec (HCI) <David.Moravec@embedit.cz>
2020-01-28 15:09:02 +0100

    Initial working version

---
+++ Get-GitRepo.ps1
@@ -0,0 +1,60 @@
+[CmdletBinding()]
+param(
+    $owner = 'Makovec',
+    $repo = 'download-git-folder',
+    $path,
+    $rootFolderName = 'git-out'
+)
+
+if( ($PSBoundParameters.ContainsKey('path')) -and 
+    (-not $PSBoundParameters.ContainsKey('rootFolderName')) )
+{
+    $rootFolderName = Split-Path $path -Leaf
+}
+
+break
+
+$rootUri = 'https://api.github.com'
+$apiCall = "/repos/{0}/{1}/contents/{2}" -f $owner, $repo, $path
+$header = @{ 
+    'Authorization' = "token $GitApiKey" 
+}
+
+$rootPath = Join-Path -Path $PSScriptRoot -ChildPath $rootFolderName
+if(-not (Test-Path -Path $rootPath))
+{
+    New-Item -Path $rootPath -ItemType Directory | Out-Null
+    Write-Verbose "Created folder $rootPath"
+}
+
+function Get-RestRec
+{
+    [CmdletBinding()]
+    param(
+        $uri
+    )
+    
+    $resp = Invoke-RestMethod -Uri $uri -Method Get -Headers $header -verbose:$false
+
+    foreach ($r in $resp)
+    {
+        if ($r.type -eq 'file')
+        {
+            Write-Verbose "File: $($r.download_url)"
+            $dPath = $r.path -replace $path, ''
+            $fullFilePath = Join-Path -Path $rootPath -ChildPath $dPath
+            Invoke-WebRequest -Uri $r.download_url -OutFile $fullFilePath -Verbose:$false
+        }
+        if ($r.type -eq 'dir')
+        {
+            $apiCall = "/repos/{0}/{1}/contents/{2}" -f $owner, $repo, $r.path
+            Write-Verbose "Dir : $($r.path)"
+            $subPath = $r.path -replace $path, ''
+            New-Item -Path $rootPath -Name $subPath -ItemType Directory -ErrorAction SilentlyContinue | Out-Null
+            Write-Verbose "  -> $apiCall"
+            Get-RestRec -uri "$rootUri$apiCall"
+        }
+    }
+}
+
+Get-RestRec -uri "$rootUri$apiCall" -Verbose
